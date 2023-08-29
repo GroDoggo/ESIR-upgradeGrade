617d8ca774eca1c9012a3f5ffeb9228eb8aa8ae5
lhuar <leopaul.huar@gmail.com>
2023-08-29 10:35:55 +0200

    execute function + upgrade intents

--- commands/upgradeRole.js
+++ commands/upgradeRole.js
@@ -23,10 +23,29 @@ module.exports = {
 async function upgradeRole(interaction) {
     await interaction.deferReply();
     const guild = interaction.guild;
-    const roleManagerCache = guild.roles.cache;
+    const baseID = interaction.options.getRole('base').id;
+    const upgradeID = interaction.options.getRole('upgrade').id;
 
-    console.log('upgradeRole: base role id: ' + interaction.options.getRole('base').id);
-    console.log('upgradeRole: upgrade role id: ' + interaction.options.getRole('upgrade').id);
+    // Fetch all members from a guild
+    await guild.members.fetch()
+        .then(members => console.log(`Fetched ${members.size} members`))
+        .catch(console.error);
 
-    await interaction.editReply('Debug was sucessful');
+    const baseRole = guild.roles.cache.get(baseID);
+    const upgradeRole = guild.roles.cache.get(upgradeID);
+
+    // Get all members with the base role
+    const members = baseRole.members;
+
+    await interaction.editReply(`Upgrading ${members.size} members...`);
+
+    //debug log all members
+    console.log('Members with base role:');
+    members.forEach(member => {
+        member.roles.add(upgradeRole);
+        member.roles.remove(baseRole);
+        console.log(member.user.username + " has been upgraded");
+    });
+
+    await interaction.editReply('Operation complete => ' + members.size + ' members upgraded');
 }
